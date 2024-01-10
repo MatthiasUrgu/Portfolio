7df94b1b27aa3dda0f0ada64f31a2e2a565d1499
Matthias.Urgu <matthias.urgu@gmail.com>
2024-01-10 15:53:19 +0100

    update
    Fix bug bckground
    beginning mobile
    @media for desktop
    Spotlight effect

--- portfolio-app/src/components/SecondContent/SecondContent.jsx
+++ portfolio-app/src/components/SecondContent/SecondContent.jsx
@@ -1,16 +1,48 @@
 import style from './SecondContent.module.scss'
 
 export default function () {
+
+  //Project informations
+  const PROJECTS = {
+    First: {
+      Title: "Body mass calculator",
+      Description :"",
+      Techno : ["React","sass"]
+    },
+    
+    Second:{
+      Title: "Weather app",
+      Description :"",
+      Techno : ["React","sass"]
+      },
+
+    Third:{
+      Title: "Beyangin",
+      Description :"",
+      Techno : ["React","sass"]
+        },
+
+    Fourth:{
+      Title: "",
+      Description :"",
+      Techno : ["React","sass"]
+        }
+  }
+
   return (
+
+    
+    
+
 
     <section className={style.sectionContent}>
             <div id="div1">
-              <h1>div 1</h1>
+              <h1>About</h1>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
             </div>
             <div id="div2">
-              <h1>div 2</h1>
+              <h1>Project</h1>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
@@ -18,7 +50,7 @@ export default function () {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
             </div>
             <div>
-              <h1>div 3</h1>
+              <h1>Contact</h1>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
