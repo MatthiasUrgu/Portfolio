e7b492857ee542e6dafa9e190eca8baa4c463124
Matthias.Urgu <matthias.urgu@gmail.com>
2024-01-11 22:26:39 +0100

    update

--- portfolio-app/src/components/SecondContent/SecondContent.jsx
+++ portfolio-app/src/components/SecondContent/SecondContent.jsx
@@ -1,79 +1,84 @@
-import style from './SecondContent.module.scss'
+import style from "./SecondContent.module.scss";
 
 export default function () {
-
   //Project informations
   const projects = [
     {
-      Id : "1",
+      Id: "1",
       Title: "Body mass calculator",
-      Description :"The Body Mass Calculator is a React.js project designed to help users assess their body weight status using the BMI calculation. It was created as a personal project with the primary goal of improving",
+      Description:
+        "The Body Mass Calculator is a React.js project designed to help users assess their body weight status using the BMI calculation. It was created as a personal project with the primary goal of improving",
       Why: "React.js skills,Enhancing my coding logic, Practicing SCSS, including concepts such as Mixins, Includes, Variables, and Responsiveness.",
-      Techno : ["React","sass"]
+      Techno: ["React", "sass"],
     },
-    
+
     {
-      Id : "2",
+      Id: "2",
       Title: "Weather app",
-      Description :"The Weather App is a simple application that provides users with real-time weather information, date, and time. It's designed for anyone who wants to quickly check the weather conditions in different locations",
-      Why : "I created this project to learn about how to use and apply APIs. The Weather App utilizes two APIs: OpenWeather for weather information and Abstract API to obtain the current UTC time.Additionally, I've incorporated Framer Motion to animate the hamburger menu for a more interactive user experience."
-      ,
-      Techno : ["React","sass","Javascript","OpenWeather Api","Abstract Api","FramerMotion"]
-      },
+      Description:
+        "The Weather App is a simple application that provides users with real-time weather information, date, and time. It's designed for anyone who wants to quickly check the weather conditions in different locations",
+      Why: "I created this project to learn about how to use and apply APIs. The Weather App utilizes two APIs: OpenWeather for weather information and Abstract API to obtain the current UTC time.Additionally, I've incorporated Framer Motion to animate the hamburger menu for a more interactive user experience.",
+      Techno: [
+        "React",
+        "sass",
+        "Javascript",
+        "OpenWeather Api",
+        "Abstract Api",
+        "FramerMotion",
+      ],
+    },
 
     {
-      Id : "3",
+      Id: "3",
       Title: "Beyangin",
-      Description :"",
-      Techno : ["React","sass"]
-      }
-
-    
-  ]
+      Description: "",
+      Techno: ["React", "sass"],
+    },
+  ];
   return (
     <section className={style.sectionContent}>
-            <div className={style.cardContent} id="div1">
-              <h1 className={style.cardSecondTitle}>
-                About
-              </h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-            </div>
-            <div className={style.cardContent} id="div2">
-              <h1 className={style.cardSecondTitle}>
-                Project
-              </h1>
-              {projects.map(project => (
-              <div id={project.Id} className={style.projectsCards} key={project.Id}>
-              <h2 className={style.cardSubtitle}>{project.Title}</h2>
-              <p className={style.cardDescription}>{project.Description}</p>
-              <p className={style.cardWhy}>{project.Why}</p>
-              <p className={style.cardTechno}>
-                <ul>
-                  {projects.map((tech, index) => (
-                  
-                      <li className={style.cardTechnoList} key={ index }>{tech.Techno}</li>
-                  
-                  ))}
-                </ul>
-              </p>
-                
-            </div>
-              ))}
-            </div>
-            <div className={style.cardContent} id="div3">
-              <h1 className={style.cardSecondTitle}>
-                Contact
-              </h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde numquam reiciendis!
-            </div>
-            <div>
-           
-            </div>
-          </section>
-  )
+      <div className={style.cardContent} id="div1">
+        <h1 className={style.cardSecondTitle}>About</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis
+        adipisci? Dolore cumque error eligendi tempora quod, unde numquam
+        reiciendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde
+        numquam reiciendis!
+      </div>
+      <div className={style.cardContent} id="div2">
+        <h1 className={style.cardSecondTitle}>Project</h1>
+        {projects.map((project) => (
+          <div id={project.Id} className={style.projectsCards} key={project.Id}>
+            <h2 className={style.cardSubtitle}>{project.Title}</h2>
+            <p className={style.cardDescription}>{project.Description}</p>
+            <p className={style.cardWhy}>{project.Why}</p>
+            <p className={style.cardTechno}>
+              <ul>
+                {projects.map((tech, index) => (
+                  <li className={style.cardTechnoList} key={ index }>
+                    {tech.Techno}
+                  </li>
+                ))}
+              </ul>
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className={style.cardContent} id="div3">
+        <h1 className={style.cardSecondTitle}>Contact</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis
+        adipisci? Dolore cumque error eligendi tempora quod, unde numquam
+        reiciendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Est, debitis adipisci? Dolore cumque error eligendi tempora quod, unde
+        numquam reiciendis! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Est, debitis adipisci? Dolore cumque error eligendi tempora quod,
+        unde numquam reiciendis! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Est, debitis adipisci? Dolore cumque error eligendi
+        tempora quod, unde numquam reiciendis! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Est, debitis adipisci? Dolore cumque error
+        eligendi tempora quod, unde numquam reiciendis!
+      </div>
+      <div></div>
+    </section>
+  );
 }
